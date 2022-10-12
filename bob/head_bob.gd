5dc394a6eb34b417afae7ab91f38a427696083f3
Rafael Correa <scriptsengineer@gmail.com>
2022-10-12 20:22:19 -0300

    Export correct head bob values

--- bob/head_bob.gd
+++ bob/head_bob.gd
@@ -4,54 +4,40 @@ class_name HeadBob
 
 @export_node_path(Camera3D) var head_path := NodePath("../Camera")
 @onready var head: Camera3D = get_node(head_path)
-var speed : float = 0
-@export var lerp_bob_curve : LerpBobCurve
-@export_group("Rotation To Move (Quake Like)")
-@export var rotation_to_move := true
-@export var speed_rotation := 4.0
-@export var angle_limit_for_rotation := 0.1
 
-var original_position
 
-func _ready():
-	original_position = head.position
-	
-func head_bob_process(horizontal_velocity:Vector3, input_axis:Vector2, is_sprint, is_on_floor:bool, _delta:float):
-	lerp_bob_curve.bob_process(_delta)
-	var headpos = do_head_bob(horizontal_velocity.length(), _delta)
-	var new_position = original_position
-	if is_on_floor:
-		new_position += headpos
-	new_position.y -= lerp_bob_curve.offset
-	head.position = new_position
-	if is_sprint:
-		input_axis *= 2
-	if rotation_to_move:
-		head_bob_rotation(input_axis.x, input_axis.y, _delta)
-		
-func head_bob_rotation(x, z, _delta):
-	var target_rotation = Vector3(x * angle_limit_for_rotation, 0.0, -z * angle_limit_for_rotation);
-	head.rotation = lerp(head.rotation, target_rotation, speed_rotation * _delta);
-
-#Lerp bob	
+@export_group("Step Bob")
+@export var step_bob_enabled := true
 @export var bob_range = Vector2(0.07, 0.07)
 @export var bob_curve : Curve
 @export var curve_multiplier = Vector2(2,2)
 
+@export_group("Jump Bob")
+@export var jump_bop_enabled := true
+@export var lerp_bob_curve : LerpBobCurve
+
+@export_group("Rotation To Move (Quake Like)")
+@export var rotation_to_move := true
+@export var speed_rotation := 4.0
+@export var angle_limit_for_rotation := 0.1
 @export var vertical_horizontal_ratio = 2
 
+var speed : float = 0
+var original_position : Vector3
+var original_rotation : Vector3
 var cycle_position_x: float = 0;
 var cycle_position_y: float = 0;
 var bob_base_interval: float = 0;
 
-func setup_bob(bob_base_interval: float):
-	self.bob_base_interval = bob_base_interval
-
-func do_bob_jump():
-	pass
-#	lerp_bob_curve.do_bob_cycle()
-
-func do_head_bob(speed: float, delta: float) -> Vector3: 
+func _ready():
+	original_position = head.position
+	original_rotation = head.rotation
+	
+func _head_bob_rotation(x, z, _delta) -> Vector3:
+	var target_rotation = Vector3(x * angle_limit_for_rotation, 0.0, -z * angle_limit_for_rotation);
+	return lerp(head.rotation, target_rotation, speed_rotation * _delta)
+	
+func _do_head_bob(speed: float, delta: float) -> Vector3:
 	var x_pos = (bob_curve.sample(cycle_position_x) * curve_multiplier.x * bob_range.x)
 	var y_pos = (bob_curve.sample(cycle_position_y) * curve_multiplier.y * bob_range.y)
 
@@ -65,6 +51,36 @@ func do_head_bob(speed: float, delta: float) -> Vector3:
 		cycle_position_y -= 1
 
 	return Vector3(x_pos,y_pos,0)
+	
+func head_bob_process(horizontal_velocity:Vector3, input_axis:Vector2, is_sprint, is_on_floor:bool, _delta:float):
+	if jump_bop_enabled:
+		lerp_bob_curve.bob_process(_delta)
+	
+	var new_position = original_position
+	var new_rotation = original_rotation
+	if step_bob_enabled:
+		var headpos = _do_head_bob(horizontal_velocity.length(), _delta)
+		if is_on_floor:
+			new_position += headpos
+			
+	if jump_bop_enabled:
+		new_position.y -= lerp_bob_curve.offset
+		
+	
+	if is_sprint:
+		input_axis *= 2
+	if rotation_to_move:
+		new_rotation += _head_bob_rotation(input_axis.x, input_axis.y, _delta)	
+	
+	head.position = new_position
+	head.rotation = new_rotation
+
+func setup_bob(bob_base_interval: float):
+	self.bob_base_interval = bob_base_interval
+
+func do_bob_jump():
+	if jump_bop_enabled:
+		lerp_bob_curve.do_bob_cycle()
 	
 func reset():
 	cycle_position_x = 0
--- bob/lerp_bob_curve.gd
+++ bob/lerp_bob_curve.gd
@@ -2,12 +2,12 @@ extends Resource
 
 class_name LerpBobCurve
 
-@export var duration = 0.2
-@export var amount = 0.1
+@export var duration := 0.2
+@export var amount := 0.1
 
 var offset = 0.0
-var direction = false
-var time = 0.0	
+var direction = true
+var time = duration
 
 func get_offset() -> float:
 	return offset
@@ -16,9 +16,9 @@ func bob_process(delta):
 	if(time > 0):
 		time -= delta
 		if(direction):
-			offset = lerp(0, amount, time/duration)
+			offset = lerp(0.0, amount, time/duration)
 		else:
-			offset = lerp(amount, 0, time/duration)
+			offset = lerp(amount, 0.0, time/duration)
 		if(time < 0 && !direction):
 			_back_do_bob_cycle()
 	
