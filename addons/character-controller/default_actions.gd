81f7bec6fd180bb47c79153a694f0d4daf246747
Rafael Correa <scriptsengineer@gmail.com>
2022-11-26 14:13:50 -0300

    Add default actions for example test

---
+++ addons/character-controller/default_actions.gd
@@ -0,0 +1,70 @@
+const CHARACTER_CONTROLLER_FORWARD := {
+	"name": "move_forward",
+	"events": [
+		{
+			"physical_keycode": KEY_W,
+		}
+	]
+}
+
+const CHARACTER_CONTROLLER_BACKWARD := {
+	"name": "move_backward",
+	"events": [
+		{
+			"physical_keycode": KEY_S,
+		}
+	]
+}
+const CHARACTER_CONTROLLER_RIGHT := {
+	"name": "move_right",
+	"events": [
+		{
+			"physical_keycode": KEY_D,
+		}
+	]
+}
+
+const CHARACTER_CONTROLLER_LEFT := {
+	"name": "move_left",
+	"events": [
+		{
+			"physical_keycode": KEY_A,
+		}
+	]
+}
+
+const CHARACTER_CONTROLLER_SPRINT := {
+	"name": "move_sprint",
+	"events": [
+		{
+			"physical_keycode": KEY_SHIFT,
+		}
+	]
+}
+
+const CHARACTER_CONTROLLER_JUMP := {
+	"name": "move_jump",
+	"events": [
+		{
+			"physical_keycode": KEY_SPACE,
+		}
+	]
+}
+
+const CHARACTER_CONTROLLER_CROUCH := {
+	"name": "move_crouch",
+	"events": [
+		{
+			"physical_keycode": KEY_CTRL,
+		}
+	]
+}
+
+const CHARACTER_CONTROLLER_FLY_MODE := {
+	"name": "move_fly_mode",
+	"events": [
+		{
+			"physical_keycode": KEY_F,
+		}
+	]
+}
