ff429305cd95e6eb3ddf507693b5302d1cdd35da
Rafael Correa <scriptsengineer@gmail.com>
2023-01-28 10:58:50 -0300

    Add setup mouse mode in ready player.gd example

--- addons/character-controller/example/main/level.gd
+++ addons/character-controller/example/main/level.gd
@@ -9,7 +9,6 @@ extends Node3D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 	
 	if !OS.is_debug_build():
 		fast_close = false
